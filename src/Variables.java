88fa5cca8f57dd64cc5d80ce91a56e21977689bd
Veranika Salabai <versoni.crow@gmail.com>
2022-11-11 20:14:01 +0100

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        //declaring new type int
+        int number;
+
+        //initialization of number variable
+        number = 10;
+
+        //Accessing number variables value
+        System.out.println(number);
+
+        //Declaring and initializing a viable at the same time
+        int age = 88;
+        System.out.println(age);
+        System.out.println("age = " + age);
+    }
+}
