9be6dad0581857000672c42e5558bd21eccd62cd
Veranika Salabai <versoni.crow@gmail.com>
2022-11-16 18:06:44 +0100

    Operators and more

---
+++ src/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Data type
+        byte num1 = 120;
+        short num2 = 10000;
+        int num3 = 457486835;
+        long num4 = 4856074835L;
+
+        //Floating point data type
+        float num5 = 1454.3467f;
+        double num6 = 454879.4485756435d;
+
+        //Boolean data type
+        boolean lightSwitch = true;
+
+        //Character data type
+        char char1 = '%';
+        char char2 = 'y';
+        char char3 = '0';
+        char char4 = '\n';
+
+        //String data type
+        String text1 = "This is a very good text";
+
+
+        //Print out variable values
+        System.out.println(num1);
+        System.out.println(num6);
+        System.out.println(lightSwitch);
+        System.out.println(char3);
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String[] args) {
+//Assignment operators =, +=, -=, *=. /=
+        int a = 100;
+        System.out.println("a = " + a);
+
+        a +=10;
+        System.out.println("a = " +a);
+    }
+}
