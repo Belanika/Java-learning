ddc7f80c17da0e7c4b29c7986fe41d285a353917
Veranika Salabai <versoni.crow@gmail.com>
2022-12-21 16:09:04 +0100

    oop

---
+++ src/oop/Triangle.java
@@ -0,0 +1,36 @@
+package oop;
+
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+    public double getSide2() {
+        return side2;
+    }
+    public double getSide3() {
+        return side3;
+    }
+
+    private boolean isValid(double s1, double s2, double s3){
+//        if(s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1){
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1;
+    }
+}
